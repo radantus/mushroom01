71bb7e682f6ace8660093fa4f4aac47e64d0c2ca
radantus <137149418+radantus@users.noreply.github.com>
2025-07-13 17:43:36 +0200

    Create useGeoLocation.ts

---
+++ src/hooks/useGeoLocation.ts
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+interface Position {
+  coords: {
+    latitude: number;
+    longitude: number;
+    accuracy: number;
+  };
+  timestamp: number;
+}
+
+export const useGeoLocation = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<GeolocationPositionError | null>(null);
+
+  const getCurrentPosition = (): Promise<Position> => {
+    return new Promise((resolve, reject) => {
+      setIsLoading(true);
+      setError(null);
+
+      if (!navigator.geolocation) {
+        const error = new Error('Geolocation is not supported by your browser');
+        setError(error as unknown as GeolocationPositionError);
+        setIsLoading(false);
+        reject(error);
+        return;
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setIsLoading(false);
+          resolve(position);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+          reject(error);
+        },
+        // Options
+        {
+          enableHighAccuracy: true,
+          timeout: 10000,
+          maximumAge: 0
+        }
+      );
+    });
+  };
+
+  return {
+    getCurrentPosition,
+    isLoading,
+    error
+  };
+};
