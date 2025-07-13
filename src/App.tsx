4b59801e167664e4d2f73db8112d029593698c78
radantus <137149418+radantus@users.noreply.github.com>
2025-07-13 18:22:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,6 @@ function App() {
           <Activity className="h-8 w-8 mr-3" />
           <h1 className="text-2xl font-bold">GeoTracker</h1>
         </div>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NBG58HVX');</script>
       </header>
       <main className="container mx-auto px-4 py-8">
         <LocationLogger />
